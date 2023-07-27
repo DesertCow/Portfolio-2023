694715b464afadaeec17463326ee9473d7ef9240
Clayton S <desertcow07@gmail.com>
2023-07-27 08:53:10 -0700

    Got bootstrap working, added photo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,8 +4,15 @@ import React, { useState } from 'react';
 function Home() {
 
   return(
-    <div>
-      <h1> Hello World</h1>
+    <div className="d-flex flex-column min-vh-100">
+      <h1 className='text-center mt-4'>Hello World...</h1>
+      <div className='text-center mt-5'>
+        <img src={require("../img/Woodz_Selfie.jpeg")}
+          className="align-items-center justify-content-center"
+          alt="Clayton Skaggs Snowy Woods"
+          width="800" height="600" />  
+      </div>
+
     </div>
   )
 
