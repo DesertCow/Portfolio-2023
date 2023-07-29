0bff5d54e718f051882854cfd1ef18c45eee01b5
Clayton S <desertcow07@gmail.com>
2023-07-29 14:04:20 -0700

    Setup material icons and worked on contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,7 +4,7 @@
 //* Component Import
 import Header from '../components/Header';
 
-
+import { ArrowRight, Camera } from '@mui/icons-material'
 
 const Contact = () => {
 
@@ -16,8 +16,21 @@ const Contact = () => {
         <Header />
       </header>
 
-      <div className='HeaderSpacer'>
-        <h1>CONTACT PAGE</h1>
+      <div className='d-flex flex-column align-items-center justify-content-center HeaderPhotoSpacer'>
+
+        <h1 className='contactHeaderText mt-5'>CONTACT PAGE</h1>        
+          <contact className='d-flex flex-column align-items-center mt-3 contactBox'>
+
+            <div className='d-flex flex-column align-items-center mt-5'>
+              Email
+            </div>
+            <div className='d-flex flex-column align-items-center mt-5 emailFont'>
+              clayton.skaggs89@gmail.com
+              <a className="mt-3 emailLinkText"href = "mailto: clayton.skaggs89@gmail.com">Send Email</a>
+              <ArrowRight />
+            </div>
+            
+          </contact>
       </div>
 
     </div>
