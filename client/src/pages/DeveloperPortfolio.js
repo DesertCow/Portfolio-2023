a9b84e49ebd43c7341a6959dcf2506af14f7bc39
Clayton S <desertcow07@gmail.com>
2023-07-31 16:56:53 -0700

    Filled out more info about projects... slowly making progress

--- client/src/pages/DeveloperPortfolio.js
+++ client/src/pages/DeveloperPortfolio.js
@@ -34,48 +34,99 @@ const Portfolio = () => {
             <h1 className="portfioloTitle p-3">Featured Projects</h1> 
           </div>
         </div>
-        <div className="py-3">
-          <div className="container text-center">
-            <div className="row portfioloDemoBox">
-              <div className="col demoText portfioloSubBoxLeft">
-                <h1 className="mt-3 demoTitle">The Board Club</h1>
-                <img className="img-fluid mt-3 mb-4 demoGIF1" src={require("../img/DemoGIFs/The_Board_Club_App_Demo1.gif")} alt="Surf Deck Demo"></img>
-                </div>
-                <div className="col d-flex portfioloSubBoxRight">
-                <div className="col softwareStackBox">
-                  <div className="mt-3">Software Stack</div>
-                  <div className="mt-3 d-flex justify-content-start">
-                    <ul className="softwareStackList text-left">
-                      <li className="py-3">&#9642; App Type: Progressive Web App</li>
-                      <li className="py-3">&#9642; Frontend: React (18.2.0)</li>
-                      <li className="py-3">&#9642; Backend: NodeJS (16.15.1)</li>
-                      <li className="py-3">&#9642; Database: MongoDB (5.6.0)</li>
-                      <li className="py-3">&#9642; Photo Host: AWS S3</li>
-                      <li className="pt-3">&#9642; App Hosting: railway.app</li>
-                      {/* <li className="pt-2">&ensp; &#9642; https://railway.app/</li> */}
-                    </ul>
-                  </div>
-                  
-                </div>
-                <div className="col">
-                  <div className="mt-3 projectDesTitles">Project Goal</div>
-                  <p>Goal of this project was </p>
-                  <div className="mt-5 projectDesTitles">Project Overview</div>
-                  <div className="mt-5 projectDesTitles">Project Highlights</div>
-                  <div className="mt-5 projectDesTitles">Project Improvements</div>
+
+        <div className="py-3 container text-center">
+
+          <div className="row portfioloDemoBox">
+            <div className="col demoText portfioloSubBoxLeft">
+              <h1 className="mt-3 demoTitle">The Board Club</h1>
+              <img className="img-fluid mt-3 mb-4 demoGIF1" src={require("../img/DemoGIFs/The_Board_Club_App_Demo1.gif")} alt="Surf Deck Demo"></img>
+            </div>
+
+            <div className="col d-flex portfioloSubBoxRight">
+              
+              <div className="col-4 softwareStackBox">
+                <div className="mt-5">Software Stack</div>
+                <div className="mt-5 d-flex justify-content-start">
+                  <ul className="softwareStackList text-left">
+                    <li className="py-3">&#9642; App Type: Progressive Web App</li>
+                    <li className="py-3">&#9642; Frontend: React (18.2.0)</li>
+                    <li className="py-3">&#9642; Framework: Bootstrap (2.5.0)</li>
+                    <li className="py-3">&#9642; Backend: NodeJS (16.15.1)</li>
+                    <li className="py-3">&#9642; Database: MongoDB (5.6.0)</li>
+                    <li className="py-3">&#9642; Photo Host: AWS S3</li>
+                    <li className="pt-3">&#9642; App Hosting: www.railway.app</li>
+                    {/* <li className="pt-2">&ensp; &#9642; https://railway.app/</li> */}
+                  </ul>
                 </div>
               </div>
-            </div>
-             <div className="row mt-5 portfioloDemoBox">
-              <div className="col demoText portfioloSubBoxLeft">
-                <h1 className="mt-3 demoTitle">BBQ Food Truck</h1>
-                <img className="img-fluid mt-3 mb-4 demoGIF1" src={require("../img/DemoGIFs/The_Board_Club_App_Demo1.gif")} alt="Surf Deck Demo"></img>
-                </div>
-              <div className="col portfioloSubBoxRight">
-                Project Info...
+
+              <div className="col">
+                <div className="mt-5 projectDesTitles">Project Overview</div>
+                  <p className="mt-3 px-5 projectDesText ">An application designed to allow members of the board club to view current surf/weather conditions, 
+                  club events, log surf sessions, view surf tips, rental information, and general information 
+                  about the club.</p>
+                
+                <div className="mt-5 projectDesTitles">Project Goal</div>
+                <p className="mt-3 px-5 projectDesText">The goal of this application to motivate to go surf by providing them with the all the infomation they need to have a great surf session.</p>
+                <ul className="projectSubDesText projectGoalText px-3 mt-3 text-center">
+                  <li className="mt-3">&#9642; The app easily shows the current surf and weather conditons. </li>
+                  <li className="mt-3">&#9642; Events are displayed to make it easy for memebers to get the information they need to plan and attend upcoming events.</li>
+                  <li className="mt-3">&#9642; The Surf Log allows memebers to keep a log of all their surf sessions to track their progression and review previous sessions conditions to use data to influence when selecting a surfboard or surf spot for future surf sessions.</li>
+                  <li className="mt-3">&#9642; Surf hacks are listed to help new members with common surf problems. </li>
+                  <li className="mt-3">&#9642; The Newport Surf map helps members easily pick the best spot to go surfing while visiting the club house based on a variety of factors and conditions.</li>
+                  <li className="mt-3">&#9642; The comminity forum helps memebers find other memebrs to surf with, sell items to other memebers, and also to interact with memebers to build a sense of community.</li>
+                </ul>
+                {/* <div className="mt-5 projectDesTitles">Project Highlights</div>
+                <div className="mt-5 projectDesTitles">Project Improvements</div> */}
               </div>
             </div>
           </div>
+
+          <div className="row portfioloDemoBox mt-5">
+            <div className="col demoText portfioloSubBoxLeft">
+              <h1 className="mt-3 demoTitle">Chuckwagon</h1>
+              <img className="img-fluid mt-3 mb-4 demoGIF1" src={require("../img/DemoGIFs/Chuckwagon_Demo1.gif")} alt="Surf Deck Demo"></img>
+            </div>
+
+            <div className="col d-flex portfioloSubBoxRight">
+              
+              <div className="col-4 softwareStackBox">
+                <div className="mt-5">Software Stack</div>
+                <div className="mt-5 d-flex justify-content-start">
+                  <ul className="softwareStackList text-left">
+                    <li className="py-3">&#9642; App Type: Progressive Web App</li>
+                    <li className="py-3">&#9642; Frontend: React (18.2.0)</li>
+                    <li className="py-3">&#9642; Framework: Bootstrap (5.2.1)</li>
+                    <li className="py-3">&#9642; Backend: NodeJS (16.15.1)</li>
+                    <li className="py-3">&#9642; Database: MongoDB (5.6.0)</li>
+                    <li className="py-3">&#9642; Database: MySQL (2.3.3)</li>
+                    <li className="pt-3">&#9642; App Hosting: www.railway.app</li>
+                    {/* <li className="pt-2">&ensp; &#9642; https://railway.app/</li> */}
+                  </ul>
+                </div>
+              </div>
+
+              <div className="col">
+                <div className="mt-5 projectDesTitles">Project Overview</div>
+                  <p className="mt-3 px-5 projectDesText ">Chuckwagon is A comprehensive Food Truck management application with a customer ordering App and a separate "In Kitchen" App (React) for order execution and application administration built using MongoDB, Express, React, and NodeJS (MERN Stack).</p>
+                
+                <div className="mt-5 projectDesTitles">Project Goal</div>
+                <p className="mt-3 px-5 projectDesText">TBD</p>
+                <ul className="projectSubDesText projectGoalText px-3 mt-3 text-center">
+                  <li className="mt-3">&#9642; TBD</li>
+                  <li className="mt-3">&#9642; TBD</li>
+                  <li className="mt-3">&#9642; TBD</li>
+                  <li className="mt-3">&#9642; TBD</li>
+                  <li className="mt-3">&#9642; TBD</li>
+                  <li className="mt-3">&#9642; TBD</li>
+                </ul>
+                {/* <div className="mt-5 projectDesTitles">Project Highlights</div>
+                <div className="mt-5 projectDesTitles">Project Improvements</div> */}
+              </div>
+            </div>
+          </div>
+
         </div>
 
         <div className="d-flex col mt-5 justify-content-center">
@@ -104,54 +155,6 @@ const Portfolio = () => {
               technologies like HTML, CSS, Javascript, React, and Node.
             </p>
           </div>  
-        </div>
-
-        <div className="container">
-          <div className="col text-center">
-
-            <div className="row d-flex container devLinksBox mt-5">
-              <div className="d-flex container flex-row py-3 align-items-center justify-content-around" >
-                
-                <div className="flex-row align-items-center justify-content-center">
-                  <a href="https://github.com/DesertCow">
-                    <img src={require('../img/Icons/github.png')}
-                      className=""
-                      width="50"
-                      alt="GitHub Icon"/>
-                  </a>
-                </div>
-                
-                <div className="flex-row align-items-center justify-content-center">
-                  <a href="https://www.linkedin.com/in/clayton-skaggs-3084b950">
-                    <img src={require('../img/Icons/LinkedIn.png')}
-                      className=""
-                      width="50"
-                      alt="LinkedIn Icon"/> 
-                  </a> 
-                </div>
-                
-                <div className="flex-row align-items-center justify-content-center">
-                  <a href="https://www.instagram.com/skagggggggs/">
-                    <img src={require('../img/Icons/Instagram.png')}
-                      className=""
-                      width="50"
-                      alt="Instagram Icon"/>
-                  </a>
-                </div>
-                
-                <div className="flex-row align-items-center justify-content-center">
-                  <a href="https://www.youtube.com/@MrClaytonskaggs">
-                    <img src={require('../img/Icons/YouTube.png')}
-                      className=""
-                      width="50"
-                      alt="YouTube Icon"/>
-                  </a>
-                </div>
-
-              </div>
-            </div>
-            <div className="w-100"></div>
-          </div>
         </div>
 
         <div className="mt-5 container d-flex flex-column align-items-center justify-content-center">
@@ -244,6 +247,54 @@ const Portfolio = () => {
         </div>
 
       </div>
+
+        <div className="container py-5">
+          <div className="col text-center">
+
+            <div className="row d-flex container devLinksBox mt-5">
+              <div className="d-flex container flex-row py-3 align-items-center justify-content-around" >
+                
+                <div className="flex-row align-items-center justify-content-center">
+                  <a href="https://github.com/DesertCow">
+                    <img src={require('../img/Icons/github.png')}
+                      className=""
+                      width="50"
+                      alt="GitHub Icon"/>
+                  </a>
+                </div>
+                
+                <div className="flex-row align-items-center justify-content-center">
+                  <a href="https://www.linkedin.com/in/clayton-skaggs-3084b950">
+                    <img src={require('../img/Icons/LinkedIn.png')}
+                      className=""
+                      width="50"
+                      alt="LinkedIn Icon"/> 
+                  </a> 
+                </div>
+                
+                <div className="flex-row align-items-center justify-content-center">
+                  <a href="https://www.instagram.com/skagggggggs/">
+                    <img src={require('../img/Icons/Instagram.png')}
+                      className=""
+                      width="50"
+                      alt="Instagram Icon"/>
+                  </a>
+                </div>
+                
+                <div className="flex-row align-items-center justify-content-center">
+                  <a href="https://www.youtube.com/@MrClaytonskaggs">
+                    <img src={require('../img/Icons/YouTube.png')}
+                      className=""
+                      width="50"
+                      alt="YouTube Icon"/>
+                  </a>
+                </div>
+
+              </div>
+            </div>
+            <div className="w-100"></div>
+          </div>
+        </div>
 
     </div>
 
