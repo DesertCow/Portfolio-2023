114604cd5a36577af85a0d6db16e7a321835d432
Clayton S <desertcow07@gmail.com>
2023-08-11 15:15:05 -0700

    Dev portfolio is done

--- client/src/pages/DeveloperPortfolio.js
+++ client/src/pages/DeveloperPortfolio.js
@@ -30,10 +30,19 @@ const Portfolio = () => {
         <Header />
       </header>
 
-      <div className='HeaderSpacer'>
+      <div className='HeaderSpacer text-center'>
         <div className="d-flex col justify-content-center">
           <div className="text-center py-5">
             <h1 className="portfioloTitle p-3">Featured Projects</h1> 
+          </div>
+        </div>
+
+        <div className="d-flex col justify-content-center">
+          <div className="text-center py-5">
+            <div className="portfioloAppTitle1 p-3">
+              <h1>Newport Board</h1> 
+              <h1 className="mt-3"> Club App</h1> 
+            </div> 
           </div>
         </div>
 
@@ -43,7 +52,7 @@ const Portfolio = () => {
           <div className="row portfioloDemoBox1">
 
             <div className="col demoText portfioloSubBoxLeft text-center">
-              <h1 className="mt-3 demoTitle">The Board Club</h1>
+              <h1 className="mt-3 demoTitle">Demo</h1>
               <img className="img-fluid mt-3 mb-4 demoGIF1" src={require("../img/DemoGIFs/The_Board_Club_App_Demo1.gif")} alt="Surf Deck Demo"></img>
             </div>
 
@@ -62,13 +71,25 @@ const Portfolio = () => {
                     <li className="py-3">&#9642; Photo Host: AWS S3</li>
                     <li className="pt-3">&#9642; App Hosting: <a href="https://boardclubapp-production.up.railway.app/">Board Club (Live)</a></li>
                     <li className="text-center my-3"><img className="img-fluid railwayLogo" src={require("../img/Icons/railway_logo.png")} alt="Railway Logo"></img></li>
-                    <li className="text-center mb-3">
-                      <a href="https://github.com/DesertCow/boardClubApp">
-                        <img src={require('../img/Icons/github.png')}
-                          className=""
-                          width="50"
-                          alt="GitHub Icon"/>
-                      </a>
+                    <li className="text-center mb-3 d-flex justify-content-around col">
+                      <div>
+                        <a href="https://github.com/DesertCow/NewportBoardClubApp_PWA">
+                          <img src={require('../img/Icons/github.png')}
+                            className=""
+                            width="50"
+                            alt="GitHub Icon"/>
+                        </a>
+                        <h1>Client</h1> 
+                      </div>
+                       <div>
+                        <a href="https://github.com/DesertCow/NewportBoardClubApp_Server">
+                          <img src={require('../img/Icons/github.png')}
+                            className=""
+                            width="50"
+                            alt="GitHub Icon"/>
+                        </a>
+                        <h1>Server</h1> 
+                      </div>
                     </li>
                   </ul>
                 </div>
@@ -83,11 +104,11 @@ const Portfolio = () => {
                 <div className="mt-5 projectDesTitles">Project Goal</div>
                 <p className="mt-3 px-5 projectDesText text-center">The goal of this application to motivate to go surf by providing them with the all the infomation they need to have a great surf session.</p>
                 <ul className="projectSubDesText projectGoalText px-3 mt-3 text-center">
-                  <li className="mt-3">&#9642; The app easily shows the current surf and weather conditons. </li>
+                  <li className="mt-3">&#9642; The app shows the current surf and weather conditons. </li>
                   <li className="mt-3">&#9642; Events are displayed to make it easy for memebers to get the information they need to plan and attend upcoming events.</li>
                   <li className="mt-3">&#9642; The Surf Log allows memebers to keep a log of all their surf sessions to track their progression and review previous sessions conditions to use data to influence decision making when selecting a surfboard or surf spot for the next surf session.</li>
                   <li className="mt-3">&#9642; Surf hacks are listed to help new members with common surf problems. </li>
-                  <li className="mt-3">&#9642; The Newport Surf map helps members easily pick the best spot to go surfing while visiting the club house based on a variety of factors and conditions.</li>
+                  <li className="mt-3">&#9642; The Newport Surf map helps members easily pick the best spot to go surfing while visiting the club house.</li>
                   <li className="mt-3">&#9642; The comminity forum helps memebers find other memebrs to surf with, sell items to other memebers, and also to interact with memebers to build a sense of community.</li>
                 </ul>
                 {/* <div className="mt-5 projectDesTitles">Project Highlights</div>
@@ -96,9 +117,16 @@ const Portfolio = () => {
             </div>
           </div>
 
+          
+          <div className="d-flex mt-5 col justify-content-center">
+            <div className="text-center mt-5 py-5">
+              <h1 className="portfioloAppTitle2 p-3">Chuckwagon</h1> 
+            </div>
+          </div>
+
           <div className="row portfioloDemoBox2">
             <div className="col demoText portfioloSubBoxLeft text-center">
-              <h1 className="mt-3 demoTitle">Chuckwagon</h1>
+              <h1 className="mt-3 demoTitle">Demo</h1>
               <img className="img-fluid mt-3 mb-4 demoGIF1" src={require("../img/DemoGIFs/Chuckwagon_Demo1.gif")} alt="Surf Deck Demo"></img>
             </div>
 
@@ -124,6 +152,7 @@ const Portfolio = () => {
                           width="50"
                           alt="GitHub Icon"/>
                       </a>
+                      <h1>Client + Server</h1>
                     </li>
                   </ul>
                 </div>
@@ -168,10 +197,10 @@ const Portfolio = () => {
             <p className='mt-4 pb-4'>
               Hello my name is Clayton,<br></br>
               &nbsp; <br></br>
-              I'm an enthusiastic engineer who is driven to develop innovative solutions. With experience in producing world class processors at Intel, I am adept at managing complex projects and ensuring they meet strict quality standards. My passion for engineering and commitment to quality make me an asset on any team striving to develop leading-edge products.
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I'm an enthusiastic computer engineer who has extensive experience developing innovative solutions. With experience in producing world class processors at Intel, I am adept at managing complex projects and ensuring they meet strict quality standards. My passion for engineering and a commitment to quality make me an asset on any team striving to develop software solutions that add real business value for customers.
               <br></br>
               &nbsp; <br></br>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; As a programmer, I excel at creating responsive and functional applications that address the needs of clients and drive business value through technical solutions. With a passion for problem-solving, I'm adept at developing software products from conception to release, delivering unique solutions that meet client requirements and exceed expectations. My skills in programming, testing and design enable me to produce products that are reliable, effective and tailored to specific business goals. With an overarching focus on delivering the best possible results for clients.
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; As a programmer, I excel at creating responsive and functional applications that address the needs of clients and drive business value through technical solutions. With a passion for problem-solving, I'm adept at developing software products from design to release, delivering unique solutions that meet client requirements and exceed expectations. My skills in programming, testing, and application design enable me to produce products that are reliable, effective, and tailored to specific business goals. With an overarching focus on delivering the best possible results for clients.
             </p>
           </div> 
         </div>
