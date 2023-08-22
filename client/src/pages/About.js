21520419afe7aad4810e37f7c6421aae59ae7489
Clayton S <desertcow07@gmail.com>
2023-08-22 10:40:24 -0700

    Proof Read changes

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -28,7 +28,7 @@ const About = () => {
                 Hello!
               </p>
               <p className="mt-3 px-5 aboutText">
-                &ensp; &ensp; My name is Clayton and I am a computer engineer with a passion for solving problems and creating amazing products. My experience working at <a href="https://www.intel.com/">Intel</a> to design and ship world class processors and has given me the skills to produce creative solutions with a focus on exceptional quaility. Transitioning these skills into the full stack development space allows me to design, build, and deploy high quaility applications that create real business values for my clients.
+                &ensp; &ensp; My name is Clayton and I am a computer engineer with a passion for solving problems and creating amazing products. My experience working at <a href="https://www.intel.com/">Intel</a> to design and ship world class processors and has given me the skills to produce creative solutions with a focus on exceptional quaility. Transitioning these skills into the full stack development space allows me to design, build, and deploy high quaility applications that create real business values for employers and clients.
               </p>
             </div>
             <div className="col">
@@ -42,7 +42,7 @@ const About = () => {
             </div>
             <div className="col aboutMeSubBoxes2 mt-4 mx-5 pb-5">
               <p className="mt-5 px-5">
-                &ensp; &ensp; I am passionate about problem solving both in my professional and personal life. This passion extends to the outdoors where I enjoy being active and testing my skills in challenging environments. During the summer months I enjoy surfing, trail running, bouldering, and rock climbing. During the winter I enjoy splitboarding, ice climbing, resort snowboarding and winter climbing/skiing expeditions. Spending time in the wilderness and pushing myself out of my comfort zone not only provides a physical challenge but also helps me further hone my problem-solving skills.
+                &ensp; &ensp; I am passionate about problem solving both in my professional and personal life. This passion extends to the outdoors where I enjoy being active and testing my skills in challenging environments. During the summer months I enjoy surfing, trail running, bouldering, and rock climbing. During the winter I enjoy splitboarding, ice climbing, snowboarding, and ice climbing/snowboard expeditions. Spending time in the wilderness and pushing myself out of my comfort zone not only provides a physical challenge but also helps me further hone my problem-solving skills.
               </p>
             </div>
           </div>
@@ -50,7 +50,7 @@ const About = () => {
           <div className="row aboutLeftBox align-items-center justify-content-start my-5">
             <div className="col aboutMeSubBoxes3 mt-4 mx-5 pb-5">
               <p className="mt-5 px-5">
-                &ensp; &ensp; Though I gained valuable experience as a hardware engineer at <a href="https://www.intel.com/">Intel</a>, my career ambitions have shifted towards software development, specifically in the cloud computing industry. Although I possess a deep understanding of the hardware side of the cloud, I now recognize the importance and potential of software development. This shift in focus has led me to pursue opportunities in software development and broaden my skillset as a full stack developer.
+                &ensp; &ensp; Though I gained valuable experience as a product development engineer at <a href="https://www.intel.com/">Intel</a>, my career ambitions have shifted towards software development, specifically in the cloud computing industry. Although I possess a deep understanding of the hardware side of the cloud, I now recognize the importance and potential of software development. This shift in focus has led me to pursue opportunities in software development and broaden my skillset as a full stack developer.
               </p>
             </div>
             <div className="col">
@@ -72,7 +72,7 @@ const About = () => {
           <div className="row aboutLeftBox align-items-center justify-content-start my-5">
             <div className="aboutMeSubBoxes2 mt-4 mx-5 pb-5">
               <p className="mt-5 px-5">
-                &ensp; &ensp; Upon realizing the potential of React in facilitating the creation of complex apps, I initially found it challenging to comprehend its programming language and environment. Despite having proficiency in over twelve programming languages, React posed a unique learning curve that demanded my full attention and effort. After attending a coding boot camp at UC Irvine I was able to overcome this technical deficient and build the foundation necessary to find success and build complex and functional applications utilizing the React framework.
+                &ensp; &ensp; Upon realizing the potential of React in facilitating the creation of complex apps, I initially found it challenging to comprehend this programming language and environment. Despite having proficiency in over twelve programming languages, React posed a unique learning curve that demanded my full attention and effort. After attending a coding boot camp at UC Irvine I was able to overcome this technical deficient and build the foundation necessary to find success and build complex and functional applications utilizing the React framework.
               </p>
             </div>
             <div className="col">
@@ -86,7 +86,7 @@ const About = () => {
             </div>
             <div className="aboutMeSubBoxes3 mt-4 mx-5 pb-5">
               <p className="mt-5 px-5">
-                &ensp; &ensp; As a developer, I strive to constantly improve and expand my skill set. I achieve this by utilizing new technologies and tools to create applications that provide tangible benefits to clients. To ensure that my code is both maintainable and deployable, I adhere to DevOps principles. Through this approach, I am able to produce high-quality code that delivers real value while constantly pushing myself to learn and grow as a developer.
+                &ensp; &ensp; As a developer, I strive to constantly improve and expand my skill set. I achieve this by utilizing new technologies and tools to create applications that provide tangible benefits to clients. To ensure that my code is both maintainable and deployable, I adhere to DevOps principles. Through this approach, I am able to produce high-quality code that delivers real value while constantly pushing myself to learn and grow as a developer. My ability to quickly learn and adapt software solutions to solve problems allows me to quickly spin up on projects and make meaningful contributions to close gaps and get applications in production.
               </p>
             </div>
           </div>
