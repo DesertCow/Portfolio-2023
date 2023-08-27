bc974bcb5d0fa8b5196f1347e83da2d20b8bb749
Clayton S <desertcow07@gmail.com>
2023-08-26 21:41:46 -0700

    Video gallery mobile refactor

--- client/src/pages/MediaPages/VideoGallery.js
+++ client/src/pages/MediaPages/VideoGallery.js
@@ -2,6 +2,7 @@
 import "./_MediaPages.css";
 
 import ReactPlayer from 'react-player'
+import React from 'react';
 
 //* Component Import
 import Header from '../../components/Header';
@@ -12,74 +13,163 @@ import Footer from '../../components/Footer';
 
 const MediaVideo = () => {
 
-  return (
+  // const [mobileUser, setMobileUser] = React.useState({
+  // // matches: window.innerWidth < 600 ? true : false,
+  // });
 
-    <div className="min-vh-100 divMain">
+  let mobileUser = false
 
-      <header className="w-100">
-        <Header />
-      </header>
+  //* Determind Full or mobile layout
+  if(window.innerWidth < 600)
+  {
+    mobileUser = true
+  }
+  else {
+    mobileUser = false
+  }
 
-      <div className="col d-flex align-items-center justify-content-center HeaderGallerySpacer">
-        <h1 className="my-4 mb-5 p-3 mediaPageTitle">Video Gallery</h1>
+  console.log(mobileUser)
+
+  if(mobileUser) {
+
+    return (
+
+      <div className="min-vh-100 divMain">
+
+        <header className="w-100">
+          <Header />
+        </header>
+
+        <div className="col d-flex align-items-center justify-content-center HeaderGallerySpacer">
+          <h1 className="my-4 mb-5 p-3 mediaPageTitle">Video Gallery Mobile</h1>
+        </div>
+
+        <div className='videoListMargin'>
+          <div className="row d-flex justify-content-center">
+            <div className="col d-flex justify-content-center">
+              <div className="col mt-3 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/mtXzsgFWdsE' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/0o9mT-q_1XU' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/jCTwBadtoBM' />
+              </div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/TZir6XfKPLQ' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/BBMliNZTb18' />
+              </div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/si2YF9yVmos' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/jeXSc4eYWyA' />
+              </div>
+            </div>
+          </div>
+
+          <div className="HeaderSpacer d-flex justify-content-center">
+          </div>
+        </div>
+
+
+        <div className="w-100">
+          <Footer />
+        </div>
+
       </div>
 
-      <div className='videoListMargin'>
-        <div className="d-flex row">
+    )
+  } 
+  else {
+
+    return (
+
+      <div className="min-vh-100 divMain">
+
+        <header className="w-100">
+          <Header />
+        </header>
+
+        <div className="col d-flex align-items-center justify-content-center HeaderGallerySpacer">
+          <h1 className="my-4 mb-5 p-3 mediaPageTitle">Video Gallery</h1>
+        </div>
+
+        <div className='videoListMargin'>
+          <div className="d-flex row">
+            <div className="row mt-5 d-flex justify-content-center">
+              <ReactPlayer className="videoMain" width="1200px" height="750px" controls={true} url='https://youtu.be/mtXzsgFWdsE' />
+            </div>
+          </div>
           <div className="row mt-5 d-flex justify-content-center">
-            <ReactPlayer className="videoMain" width="1200px" height="750px" controls="true" url='https://youtu.be/mtXzsgFWdsE' />
-          </div>
-        </div>
-        <div className="row mt-5 d-flex justify-content-center">
-          <div className="col d-flex justify-content-center">
-            <div className="col mt-5 d-flex justify-content-center">
-              <ReactPlayer width="800px" height="540px" controls="true" url='https://youtu.be/0o9mT-q_1XU' />
+            <div className="col d-flex justify-content-center">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/0o9mT-q_1XU' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/jCTwBadtoBM' />
+              </div>
             </div>
           </div>
-          <div className="col">
-            <div className="col mt-5 d-flex justify-content-center">
-              <ReactPlayer width="800px" height="540px" controls="true" url='https://youtu.be/jCTwBadtoBM' />
+          <div className="row mt-5">
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/TZir6XfKPLQ' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/BBMliNZTb18' />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col">
-            <div className="col mt-5 d-flex justify-content-center">
-              <ReactPlayer width="800px" height="540px" controls="true" url='https://youtu.be/TZir6XfKPLQ' />
+          <div className="row mt-5">
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/si2YF9yVmos' />
+              </div>
+            </div>
+            <div className="col">
+              <div className="col mt-5 d-flex justify-content-center">
+                <ReactPlayer width="400px" height="260px" controls={true} url='https://youtu.be/jeXSc4eYWyA' />
+              </div>
             </div>
           </div>
-          <div className="col">
-            <div className="col mt-5 d-flex justify-content-center">
-              <ReactPlayer width="800px" height="540px" controls="true" url='https://youtu.be/BBMliNZTb18' />
-            </div>
-          </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col">
-            <div className="col mt-5 d-flex justify-content-center">
-              <ReactPlayer width="800px" height="540px" controls="true" url='https://youtu.be/si2YF9yVmos' />
-            </div>
-          </div>
-          <div className="col">
-            <div className="col mt-5 d-flex justify-content-center">
-              <ReactPlayer width="800px" height="540px" controls="true" url='https://youtu.be/jeXSc4eYWyA' />
-            </div>
+
+          <div className="HeaderSpacer d-flex justify-content-center">
           </div>
         </div>
 
-        <div className="HeaderSpacer d-flex justify-content-center">
+
+        <div className="w-100">
+          <Footer />
         </div>
+
       </div>
 
-
-      <div className="w-100">
-        <Footer />
-      </div>
-
-    </div>
-
-  )
-
+    )
+  }
 }
 
 export default MediaVideo;
