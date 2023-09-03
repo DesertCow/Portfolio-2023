875ad531e7407af8f4911d710ca61f8637f122ff
Clayton S <desertcow07@gmail.com>
2023-09-02 21:17:47 -0700

    typo fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -33,7 +33,7 @@ function Home() {
             &ensp; &ensp;&ensp; &ensp; As a Computer Engineering graduate from Oklahoma State University and former Product Development Engineer at <a href="https://www.intel.com/">Intel Corp</a>, I have a solid technical foundation and more than five years of experience developing software solutions while working on complex silicon engineering projects with a focus on exceptional quality and meeting market driven deadlines.
             <br></br>
             <br></br>
-            &ensp; &ensp;&ensp; &ensp; I am seeking to apply my engineering problem solving skills to excel as a Full Stack developer. With a keen eye for detail and a drive to contiuously grow my skills while also learning new and emerging technologies. I am confident in my ability to solve problems, meet deadlines, and deliver exceptional results that exceed a clients or employers expectations.
+            &ensp; &ensp;&ensp; &ensp; I am seeking to apply my engineering problem solving skills to excel as a Full Stack developer. With a keen eye for detail and a drive to contiuously grow my skills while also learning new and emerging technologies. I am confident in my ability to solve problems, meet commitments, and deliver exceptional results that exceed a clients or employers expectations.
           </div> 
 
         </div>
