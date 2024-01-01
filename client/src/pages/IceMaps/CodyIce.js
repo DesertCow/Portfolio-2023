01ecd90ea59eb027c35582560f60337d0cd3d070
Clayton Skaggs <desertcow07@gmail.com>
2023-12-31 16:08:31 -0800

    Added description and link to googleEarth file hosted on S3

--- client/src/pages/IceMaps/CodyIce.js
+++ client/src/pages/IceMaps/CodyIce.js
@@ -28,17 +28,39 @@ const CodyIce = () => {
           <img className="col img-fluid codyMap px-5" src={CodyMapOverview} alt="Cody Ice Climbing Overview"></img>
         </a>
         
-        <p className="col px-5 codyDescriptionText">
-          The South Fork of the Shoshone River, 35 miles southwest of Cody, Wyoming, is home to the highest concentration of frozen
-          waterfalls in the continental United States. During a typical season there will be over 150 pitches of ice within a 10-mile 
-          radius. When conditions are exceptional you can expect over 300 pitches of ice. South Fork offers world class multiptich ice 
-          climbing with generally low snow accumulations making travel relatively easy compared to other ice climbing venues. With the high
-          density of climbs and relativly long drive to Cody from large population centers the South Fork is rarely "busy" and offers an intimate
-          climbing experience.
+        <div className="row align-items-center my-5">
+          <div className="col my-5 aboutAreaBox">
+            {/* <p className="col px-5 codyDescriptionText"> */}
+            <p className="m-3 p-3">
+            &emsp; The South Fork of the Shoshone River which is 35 miles southwest of Cody Wyoming is home to the highest concentration of frozen
+            waterfalls in the continental United States. 
+            <br></br>
+            <br></br>
+            &emsp; During a typical season there will be over 150 pitches of ice within a 10-mile 
+            radius. When conditions are exceptional you can expect over 300 pitches of ice. 
+            <br></br>
+            <br></br>
+            &emsp; South Fork offers world class multiptich ice climbing with generally low snow accumulations making travel relatively easy 
+            compared to other ice climbing venues. 
+            <br></br>
+            <br></br>
+            &emsp; With the high
+            density of climbs and relativly long drive to Cody from large population centers the South Fork is rarely "busy" and offers an intimate
+            climbing experience.
         </p>
+          </div>
+        </div>
+
       </div>
       
-
+      <div>
+      <div className="col d-flex align-items-center justify-content-center mt-5">
+        <a className="googleEarthDownloadLink" href="https://cskaggs-google-earth-bucket-2017.s3.us-west-1.amazonaws.com/GE_Network_Link/Cody_V0p5.kmz">
+          <h1 className="my-4 mb-5 p-3 googleEarthDownloadBox">Google Earth Download</h1>
+        </a>
+      </div>
+        
+      </div>
 
       <div className="w-100">
         <Footer />
